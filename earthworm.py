935b7340d43e84bef4d506f86ee0254098b37843
Peter van Heusden <pvh@sanbi.ac.za>
2018-03-13 09:43:19 +0200

    Added find_real_words

--- earthworm.py
+++ earthworm.py
@@ -48,3 +48,10 @@ for line in page:
     new_word_count = len(word_list) + word_count # count the number of words in word_list and add to word_count
     word_count = new_word_count # store the new total word_count in word_count
 print("words in book:", word_count) #  print the word count
+
+def find_real_words(word_list):
+    new_word_list = []
+    for word in word_list:
+        if is_word(word):
+            new_word_list.append(word)
+    return new_word_list
